a4bda92a5e9df12e79ab75174a1fa2063f1ffdf5
cool <wshychbydh@gmail.com>
2018-04-12 09:00:07 +0800

    gradle compile->implement

--- action/src/main/java/com/plugin/monitor/util/ThreadUtils.kt
+++ action/src/main/java/com/plugin/monitor/util/ThreadUtils.kt
@@ -6,7 +6,7 @@ import java.util.concurrent.Executors
  * Created by cool on 2018/3/26.
  */
 internal object ThreadUtils {
-    private val executors = Executors.newFixedThreadPool(3)
+    private val executors = Executors.newFixedThreadPool(2)
     fun execute(R: () -> Unit) {
         executors.execute(R)
     }
