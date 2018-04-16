cb855fbfc48b5b2f73e486d26aa748bacb187f3d
cool <wshychbydh@gmail.com>
2018-04-16 11:23:57 +0800

    modify sqlte's onupgrade

--- action/src/main/java/com/plugin/monitor/db/helper/SqliteHelper.kt
+++ action/src/main/java/com/plugin/monitor/db/helper/SqliteHelper.kt
@@ -9,16 +9,15 @@ import com.plugin.monitor.MonitorSdk.context
  * Created by cool on 2018/3/22.
  */
 internal class SqliteHelper(context: Context?, name: String?, factory: SQLiteDatabase.CursorFactory?, version: Int) : SQLiteOpenHelper(context, name, factory, version) {
-    private val CREATE_TABLE = ("create table $TABLE_NAME ("
-            + "$ID integer,"
-            + "$BODY text)")
 
     override fun onCreate(db: SQLiteDatabase?) {
         createTable(db)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        //FIXME
+        //FIXME bad design
+        db?.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        createTable(db)
     }
 
     private fun createTable(db: SQLiteDatabase?) {
@@ -26,11 +25,15 @@ internal class SqliteHelper(context: Context?, name: String?, factory: SQLiteDat
     }
 
     companion object {
-        private val DATABASE_NAME = "monitor.db"
-        private val DATABASE_VERSION = 1
+
+        private const val DATABASE_NAME = "monitor.db"
+        private const val DATABASE_VERSION = 1
         const val TABLE_NAME = "ACTIONS"
         const val BODY = "body"
         const val ID = "id"
+        private const val CREATE_TABLE = ("create table $TABLE_NAME ("
+                + "$ID integer,"
+                + "$BODY text)")
 
         private var instance: SQLiteOpenHelper? = null
 
